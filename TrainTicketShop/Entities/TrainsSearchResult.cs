e6a20481f5dd18f421a654fc20ffa84f01439f3c
Interstell <urukovdima@gmail.com>
2017-04-24 00:18:01 +0300

    no-trains bug fixed

--- TrainTicketShop/Entities/TrainsSearchResult.cs
+++ TrainTicketShop/Entities/TrainsSearchResult.cs
@@ -11,59 +11,61 @@ namespace TrainTicketShop.Entities {
         public TrainsSearchResult(string json) {
             dynamic results = JsonConvert.DeserializeObject<dynamic>(json);
             this.Trains = new List<TrainBriefly>();
-            foreach(dynamic rawTrain in results.trains) {
-                TrainBriefly train = new TrainBriefly {
-                    Hash = rawTrain.hash,
-                    Number = rawTrain.number,
-                    IsAbroad = rawTrain.isAbroad,
-                    TravelTime = rawTrain.travelTime,
-                    PassengerDepartureDate = rawTrain.passengerDepartureDate,
-                    PassengerArrivalDate = rawTrain.passengerArrivalDate,
-                    DepartureStationId = rawTrain.departureStationId,
-                    ArrivalStationId = rawTrain.arrivalStationId,
-                    PassengerDepartureStationId = rawTrain.passengerDepartureStationId,
-                    PassengerArrivalStationId = rawTrain.passengerArrivalStationId,
-                    IsElectronic = rawTrain.isElectronic,
-                    IsRouteExists = rawTrain.isRouteExists,
-                    Class_ = new Description {
-                        Id = rawTrain.class_.id,
-                        Name = rawTrain.class_.name
-                    },
-                    Category = new Description {
-                        Id = rawTrain.category.id,
-                        Name = rawTrain.category.name
-                    },
-                    Speed = new Description {
-                        Id = rawTrain.speed.id,
-                        Name = rawTrain.speed.name
-                    },
-                    Model = new Description {
-                        Id = rawTrain.model.id,
-                        Name = rawTrain.model.name
+            if (results.trains != null) {
+                foreach (dynamic rawTrain in results.trains) {
+                    TrainBriefly train = new TrainBriefly {
+                        Hash = rawTrain.hash,
+                        Number = rawTrain.number,
+                        IsAbroad = rawTrain.isAbroad,
+                        TravelTime = rawTrain.travelTime,
+                        PassengerDepartureDate = rawTrain.passengerDepartureDate,
+                        PassengerArrivalDate = rawTrain.passengerArrivalDate,
+                        DepartureStationId = rawTrain.departureStationId,
+                        ArrivalStationId = rawTrain.arrivalStationId,
+                        PassengerDepartureStationId = rawTrain.passengerDepartureStationId,
+                        PassengerArrivalStationId = rawTrain.passengerArrivalStationId,
+                        IsElectronic = rawTrain.isElectronic,
+                        IsRouteExists = rawTrain.isRouteExists,
+                        Class_ = new Description {
+                            Id = rawTrain.class_.id,
+                            Name = rawTrain.class_.name
+                        },
+                        Category = new Description {
+                            Id = rawTrain.category.id,
+                            Name = rawTrain.category.name
+                        },
+                        Speed = new Description {
+                            Id = rawTrain.speed.id,
+                            Name = rawTrain.speed.name
+                        },
+                        Model = new Description {
+                            Id = rawTrain.model.id,
+                            Name = rawTrain.model.name
+                        }
+                    };
+                    train.CarTypes = new List<CarType>();
+                    foreach (dynamic rawCarType in rawTrain.carTypes) {
+                        train.CarTypes.Add(
+                                new CarType {
+                                    Id = rawCarType.id,
+                                    Name = rawCarType.name,
+                                    FreeSeats = rawCarType.freeSeats,
+                                    EstimatedTax = rawCarType.estimatedTax,
+                                    NoTaxData = Double.Parse((string)(rawCarType.estimatedTax), System.Globalization.CultureInfo.InvariantCulture) == 0
+                                }
+                            );
                     }
-                };
-                train.CarTypes = new List<CarType>();
-                foreach(dynamic rawCarType in rawTrain.carTypes) {
-                    train.CarTypes.Add(
-                            new CarType {
-                                Id = rawCarType.id,
-                                Name = rawCarType.name,
-                                FreeSeats = rawCarType.freeSeats,
-                                EstimatedTax = rawCarType.estimatedTax,
-                                NoTaxData = Double.Parse((string)(rawCarType.estimatedTax), System.Globalization.CultureInfo.InvariantCulture) == 0
+                    Trains.Add(train);
+                }
+                Stations = new List<StationBriefly>();
+                foreach (dynamic station in results.stations) {
+                    Stations.Add(
+                            new StationBriefly {
+                                Id = station.id,
+                                Name = station.name
                             }
                         );
                 }
-                Trains.Add(train);
-            }
-            Stations = new List<StationBriefly>();
-            foreach(dynamic station in results.stations) {
-                Stations.Add(
-                        new StationBriefly {
-                            Id = station.id,
-                            Name = station.name
-                        }
-                    );
             }
         }
     }
